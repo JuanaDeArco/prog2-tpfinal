77c4505cc723ae82130c63778091e220eab2e552
Lucia Vazquez <131535537+LuciaVazquez08@users.noreply.github.com>
2024-10-13 15:26:27 -0300

    base

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "ACA VA LO QUE DEVUELVE CUANDO UNO VA A LA PAG -- LOGIN"
+    return "ACA VA LO QUE DEVUELVE CUANDO UNO VA A LA PAG -- LOGIN?"
+
+@app.route("/login")
+def login():
+    return "quiero loggearme"
+
+@app.route("/login/user")
+def mi_ferfil():
+    return "mi perfil"
 
 api = Api(app)
 
